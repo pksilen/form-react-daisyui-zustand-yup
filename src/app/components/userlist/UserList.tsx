cf336c448396448018dca6a97522a894a9bd742d
Petri Silen <petri.silen@nokia.com>
2024-05-13 13:07:52 +0300

    Update

--- src/app/components/userlist/UserList.tsx
+++ src/app/components/userlist/UserList.tsx
@@ -8,13 +8,13 @@ export const UserList = () => {
       <li
         className="ml-6 list-disc"
         key={id}
-      >{`${firstName}, ${lastName}, ${streetAddress}, ${zipCode}, ${city}, ${email} ${phoneNumber}`}</li>
+      >{`${firstName}, ${lastName}, ${streetAddress}, ${zipCode}, ${city}, ${email}, ${phoneNumber}`}</li>
     )
   );
 
   return (
     <section>
-      <h2 className="mb-5 text-3xl font-bold ">Users</h2>
+      <h2 className="mb-5 text-3xl font-bold">Users</h2>
       <ul>{userListItems}</ul>
     </section>
   );
--- src/app/services/FakeUserService.ts
+++ src/app/services/FakeUserService.ts
@@ -7,11 +7,12 @@ class FakeUserService implements UserService {
   private readonly users: User[] = [];
 
   registerUser(user: UserSchema): Promise<User> {
-    const registerUser = { id: uuidv4(), ...user };
+    const registeredUser = { id: uuidv4(), ...user };
+    const isTest = window.location.href.includes('test');
 
-    if (Math.random() < 0.7) {
-      this.users.push(registerUser);
-      return Promise.resolve(registerUser);
+    if ((isTest && !window.location.href.includes('fail')) || (!isTest && Math.random() < 0.7)) {
+      this.users.push(registeredUser);
+      return Promise.resolve(registeredUser);
     }
 
     return Promise.reject(new Error());
